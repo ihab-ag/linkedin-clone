2e5b46314033016b666beea74f6dbca61e7760fd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-15 22:58:16 +0300

    <backend> feat: update user router

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
-const { getUser } = require('../controllers/user.controller')
+const { getUser, updateUser } = require('../controllers/user.controller')
 const auth = require('../middleware/auth.middleware')
 const user = require('../middleware/user.middleware')
 const router = Router()
 
 router.get('/', auth, user, getUser)
+
+router.put('/', auth, user, updateUser)
