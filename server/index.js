dbf11f60f8bfd4255ba970a5d6b4cac8c65983bd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-15 19:57:03 +0300

    <backend> config(index): auth routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(express.json());
 
+const authRouter = require('./routes/auth.routes')
+app.use('/auth', authRouter)
+
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
