c44080a3685739b5747b39530e761ca69228534d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-15 22:56:21 +0300

    <backend> feat(user controller): update user

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,6 +8,24 @@ const getUser = async (req, res) => {
     res.json(user)
 }
 
+const updateUser = async (req, res) => {
+    const id = req.id
+    const {name, bio, experience} = req.body
+
+    const user = await User.findById(id)
+
+    user.name = name
+    user.cv.bio = bio
+    user.cv.experience = experience == null ? [] : experience
+
+    await user.save()
+
+    const userNew = await User.findById(id)
+    res.json(user)
+    
+}
+
 module.exports = {
-    getUser
+    getUser,
+    updateUser
 }
