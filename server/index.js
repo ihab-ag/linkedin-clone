8e586da5cfbdb852dcf15ef0456612f93b7810fd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-16 15:37:56 +0300

    <backend> config: setup up routes

--- server/index.js
+++ server/index.js
@@ -9,10 +9,12 @@ app.use(express.json());
 const authRouter = require('./routes/auth.routes')
 app.use('/auth', authRouter)
 
-const auth = require('./middleware/auth.middleware')
-const user = require('./middleware/user.middleware')
-const {getUser} = require('./controllers/user.controller')
-app.get('/test', auth , user, getUser)
+const userRouter = require('./routes/user.routes')
+app.use('/user', userRouter)
+
+const companyRouter = require('./routes/company.routes')
+app.use('/company', companyRouter)
+
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
