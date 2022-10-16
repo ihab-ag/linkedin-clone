75aef588c83de7f18da9e5e6ac85889a2f03bb94
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-17 00:14:17 +0300

    <frontend> feat: sign up rote

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Entry from './components/layouts/entry.layout';
 import Login from './components/login';
+import { Signup } from './components/signup';
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Entry />} >
           <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
         </Route>
-        
       </Routes>
     </Router>
   );
