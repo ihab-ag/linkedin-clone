d1a6af33e5f843c1b05c3e1cf84b61a6aa0dd661
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-16 21:50:21 +0300

    <frontend> feat: login route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Entry from './components/layouts/entry.layout';
+import Login from './components/login';
 
 function App() {
   return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Entry />} >
+          <Route path='login' element={<Login />} />
+        </Route>
+        
+      </Routes>
+    </Router>
   );
 }
 
