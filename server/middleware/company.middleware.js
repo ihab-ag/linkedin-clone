428a59fa611817491f8940eaa7f984a82bd53a24
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-16 11:04:55 +0300

    <backend> feat: company middleware

---
+++ server/middleware/company.middleware.js
@@ -0,0 +1,9 @@
+
+const companyMiddleware = (req, res, next) => {
+    const TYPE = 'company'
+    const type = req.type
+
+    type === TYPE ? next() : res.status(401).send('unauthorized')
+}
+
+module.exports = userMiddleware
