652853479484eeca2875f2bf2bceb30af2e008aa
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-16 23:26:04 +0300

    <backend> config: cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
-const express = require('express');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 require('./config/db.config')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 const authRouter = require('./routes/auth.routes')
 app.use('/auth', authRouter)
