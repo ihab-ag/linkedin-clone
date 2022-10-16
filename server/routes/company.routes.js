a52c078f364916bee7f1312ae88579c515cb2193
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-16 15:34:11 +0300

    <backend> feat: get user route

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { postJob, getJobs } = require('../controllers/company.controller')
+const { postJob, getJobs, getUser } = require('../controllers/company.controller')
 const auth = require('../middleware/auth.middleware')
 const company = require('../middleware/company.middleware')
 const router = Router()
@@ -7,3 +7,5 @@ const router = Router()
 router.post('/job', auth, company, postJob)
 
 router.get('/jobs', auth, company, getJobs)
+
+router.get('/user/:user_id', auth, company, getUser)
