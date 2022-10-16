d2bf1c2963ba51b52b4e97b543224cfe8645958f
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-16 11:48:18 +0300

    <backend> feat(company controller): notifications on job post

--- server/controllers/company.controller.js
+++ server/controllers/company.controller.js
@@ -1,5 +1,7 @@
 const Company = require('../models/company.model')
 const Job = require('../models/job.model')
+const Following =require('../models/following.model')
+const Notifications = require('../models/notifications.model')
 
 const postJob = async (req, res) => {
 
@@ -24,7 +26,28 @@ const postJob = async (req, res) => {
 
         await company.save()
 
-        res.json(job)
+        const users = await Following.find({"companies": company})
+
+        for(const user of users){
+            
+            let userNotifications = await Notifications.findOne({'user' : user.user,});
+
+            if(!userNotifications){
+
+                userNotifications = new Notifications
+                userNotifications.user = user.user
+                userNotifications.jobs = [job]
+
+                await userNotifications.save()
+                
+                continue
+            }
+
+            userNotifications.jobs= [...userNotifications.jobs, job]
+            await userNotifications.save()
+        }
+
+        res.json(users)
 
     }catch(error){
         res.status(404).send(error.message)
