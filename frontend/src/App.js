f1a0ceb8162a20869fac9b61d9498a323b492dbc
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-17 13:32:56 +0300

    <frontend> config: react query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Entry from './components/layouts/entry.layout';
 import Login from './components/pages/login';
 import { Main } from './components/pages/main';
 import { Signup } from './components/pages/signup';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient()
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Entry />} >
-          <Route path='login' element={<Login />} />
-          <Route path='signup' element={<Signup />} />
-        </Route>
-        <Route path='main' element={<Main />}>
-        </Route>
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient} >
+      <Router>
+        <Routes>
+          <Route path='/' element={<Entry />} >
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+          </Route>
+          <Route path='main' element={<Main />}>
+          </Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
