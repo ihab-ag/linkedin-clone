56d86689c73f5ca40a710e305250803e3a2452f8
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-18 12:15:33 +0300

    <frontend> config: export

--- frontend/src/apis/update-user.api.js
+++ frontend/src/apis/update-user.api.js
@@ -3,6 +3,6 @@ import { putReq } from "./config/axios.config"
 const route = 'user'
 const token =localStorage.getItem('token')
 
-const updateUserReq = (data) => {
+export const updateUserReq = (data) => {
     return putReq(route,data,token)
 }
