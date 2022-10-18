ed257e1b02e4c1e5ddf3af6faa7dcb9f5287173a
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-18 11:18:55 +0300

    <frontend> feat: axios put request

--- frontend/src/apis/config/axios.config.js
+++ frontend/src/apis/config/axios.config.js
@@ -13,6 +13,17 @@ export const postReq = async (route,data,token=null) =>{
     }
 }
 
+export const putReq = async (route,data,token=null) =>{
+    try{
+        return await axios.put(base_url+route,data,{
+            headers: {
+            'Authorization': 'Bearer ' + token
+          }});
+    }catch(error) {
+        return error;
+    }
+}
+
 export const getReq = (route,token=null) =>{
     try{
          return axios.get(base_url+route,{
