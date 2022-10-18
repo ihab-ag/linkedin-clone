43fba940d7773c754dacd636a1b0b1538c78ca9f
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-18 13:57:54 +0300

    <frontend> fix: removed unneeded param

--- frontend/src/apis/get-user.api.js
+++ frontend/src/apis/get-user.api.js
@@ -3,6 +3,6 @@ import { getReq } from "./config/axios.config"
 const route = 'user'
 const token = localStorage.getItem('token')
 
-export const getUserReq = (data) => {
-    return getReq(route, data, token)
+export const getUserReq = () => {
+    return getReq(route, " ", token)
 }
