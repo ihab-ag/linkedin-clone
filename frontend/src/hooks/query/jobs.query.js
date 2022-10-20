1149652cd4181fa80641a5fc3250f2e6c0f018b0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 20:55:22 +0300

    <frontend> feat: get jobs query

---
+++ frontend/src/hooks/query/jobs.query.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import { getJobsReq } from "../../apis/get-jobs.api"
+
+
+export const USER_KEY = ["USER_KEY"]
+
+export const useJobs = (search) => useQuery({
+
+    refetchOnWindowFocus: false,
+    queryKey: USER_KEY,
+    queryFn: () => getJobsReq(search),
+    staleTime: Infinity
+})
