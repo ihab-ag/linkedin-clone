010d6f170444b7b2ba8531544de512de3d370ce9
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 22:45:03 +0300

    <frontend> fix: apply job function name

--- frontend/src/apis/apply-job.api.js
+++ frontend/src/apis/apply-job.api.js
@@ -3,6 +3,6 @@ import { postReq } from "./config/axios.config";
 const route="user/job"
 const token= localStorage.getItem('token')
 
-export const loginReq= (data) =>{
+export const applyJobReq= (data) =>{
    return postReq(route,data,token);
 }
