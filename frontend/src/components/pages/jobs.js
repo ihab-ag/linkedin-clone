09fec14c464ed2ffdbb9d8121247df7ec9e9f988
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 22:10:57 +0300

    <frontend> fix: removed jobs query

--- frontend/src/components/pages/jobs.js
+++ frontend/src/components/pages/jobs.js
@@ -1,25 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { useJobs } from '../../hooks/query/jobs.query'
+import React, { useState } from 'react'
 import { Job } from '../ui/job'
 
 export const Jobs = () => {
     const [search, setSearch] = useState('')
-    const { data, isFetching, refetch } = useJobs(search)
 
-    useEffect(() => {
-
-        if (!isFetching) {
-            console.log(data)
-        }
-
-    }, [isFetching,search])
     return (
         <>
             <form>
                 <h3 className=" text-lg font-bold ">Search</h3>
-                <input type="text" class="border-solid border-cyan-600 border-2 rounded-md " value={search} onChange={(e) => { setSearch(e.target.value); refetch()}} />
+                <input type="text" class="border-solid border-cyan-600 border-2 rounded-md " value={search} onChange={(e) => { setSearch(e.target.value)}} />
             </form>
-            <Job />
+            <Job search={search}/>
         </>
     )
 }
