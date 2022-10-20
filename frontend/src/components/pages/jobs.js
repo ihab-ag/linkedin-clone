a715262ba2378826f40db3f8fbe3e859cce8b93e
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 21:09:20 +0300

    <frontend> feat: fetch jobs matches

--- frontend/src/components/pages/jobs.js
+++ frontend/src/components/pages/jobs.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useJobs } from '../../hooks/query/jobs.query'
 import { Job } from '../ui/job'
 
 export const Jobs = () => {
-    const [search,setSearch] = useState('')
+    const [search, setSearch] = useState('')
+    const { data, isFetching, refetch } = useJobs(search)
 
-  return (
-    <>
-    <form>
-        <h3 className=" text-lg font-bold ">Search</h3>
-        <input type="text" class="border-solid border-cyan-600 border-2 rounded-md " value={search} onChange={(e) => setSearch(e.target.value)} />
-    </form>
-    <Job />
-    </>
-  )
+    useEffect(() => {
+
+        if (!isFetching) {
+            console.log(data)
+        }
+
+    }, [isFetching,search])
+    return (
+        <>
+            <form>
+                <h3 className=" text-lg font-bold ">Search</h3>
+                <input type="text" class="border-solid border-cyan-600 border-2 rounded-md " value={search} onChange={(e) => { setSearch(e.target.value); refetch()}} />
+            </form>
+            <Job />
+        </>
+    )
 }
