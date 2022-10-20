576b95473ee0eb50c882f5c7fdc2bb90ef73aaa7
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 23:47:17 +0300

    <frontend> fix: query key

--- frontend/src/hooks/query/jobs.query.js
+++ frontend/src/hooks/query/jobs.query.js
@@ -2,12 +2,12 @@ import { useQuery } from "@tanstack/react-query"
 import { getJobsReq } from "../../apis/get-jobs.api"
 
 
-export const USER_KEY = ["JOBS_KEY"]
+export const JOBS_KEY = ["JOBS_KEY"]
 
 export const useJobs = (search) => useQuery({
 
     refetchOnWindowFocus: false,
-    queryKey: USER_KEY,
+    queryKey: JOBS_KEY,
     queryFn: () => getJobsReq(search),
     staleTime: Infinity
 })
