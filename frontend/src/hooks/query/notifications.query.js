52332efee446a9298c16f5f5706e2f46dac7b180
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 23:57:08 +0300

    <frontend> fix: hook name

--- frontend/src/hooks/query/notifications.query.js
+++ frontend/src/hooks/query/notifications.query.js
@@ -4,7 +4,7 @@ import { getNotificationsReq } from "../../apis/get-notifications.api"
 
 export const NOTIFICATIONS_KEY = ["NOTIFICATIONS_KEY"]
 
-export const useUser = () => useQuery({
+export const useNotifications = () => useQuery({
 
     refetchOnWindowFocus: false,
     queryKey: NOTIFICATIONS_KEY,
