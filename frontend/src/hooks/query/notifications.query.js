7187e0b2b8764105338577b779ab9cbf5cd39e66
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-20 23:46:33 +0300

    <frontend> feat: get notifications query

---
+++ frontend/src/hooks/query/notifications.query.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import { getNotificationsReq } from "../../apis/get-notifications.api"
+
+
+export const NOTIFICATIONS_KEY = ["NOTIFICATIONS_KEY"]
+
+export const useUser = () => useQuery({
+
+    refetchOnWindowFocus: false,
+    queryKey: NOTIFICATIONS_KEY,
+    queryFn: () => getNotificationsReq,
+    staleTime: Infinity
+})
